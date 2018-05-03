b1dcffbe91d72dbc1900109427b6bbd680e1dade
Xun Guo <gxlance@gmail.com>
2018-05-04 01:00:11 +1000

    Add sinon. Some more test for shallow testing

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import _ from 'lodash'
 
+import sinon from 'sinon'
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
@@ -8,7 +9,7 @@ import ZoomSlideBar from '../src'
 
 Enzyme.configure({ adapter: new Adapter() })
 
-test('Initialise ZoomSlideBar state after mount', () => {
+describe('Initialise ZoomSlideBar', () => {
   const low = 0
   const high = 100
   const max = 100
@@ -16,33 +17,50 @@ test('Initialise ZoomSlideBar state after mount', () => {
   const step = 1
   const mockFn = jest.fn()
 
-  const slideBar = Enzyme.shallow(
-    <ZoomSlideBar
-      Low={low}
-      High={high}
-      Max={max}
-      Min={min}
-      onChangeLow={mockFn}
-      onChangeHigh={mockFn}
-      onChangePos={mockFn} />
-  )
+  it('calls componentDidMount', () => {
+    sinon.spy(ZoomSlideBar.prototype, 'componentDidMount')
+    Enzyme.shallow(
+      <ZoomSlideBar
+        Low={low}
+        High={high}
+        Max={max}
+        Min={min}
+        onChangeLow={mockFn}
+        onChangeHigh={mockFn}
+        onChangePos={mockFn} />
+    )
+    expect(ZoomSlideBar.prototype.componentDidMount.calledOnce).toBe(true);
+  })
 
-  // Low pos object
-  expect(slideBar.state('LowPos').Value).toBe(low)
-  expect(slideBar.state('LowPos').Label).toBe(`${low}`)
-  // expect(slideBar.state('LowPos').Percentage).toBe(0)
+  it('Initialise ZoomSlideBar state after mount', () => {
+    const slideBar = Enzyme.shallow(
+      <ZoomSlideBar
+        Low={low}
+        High={high}
+        Max={max}
+        Min={min}
+        onChangeLow={mockFn}
+        onChangeHigh={mockFn}
+        onChangePos={mockFn} />
+    )
 
-  // High pos object
-  expect(slideBar.state('HighPos').Value).toBe(high)
-  expect(slideBar.state('HighPos').Label).toBe(`${high}`)
-  // expect(slideBar.state('HighPos').Percentage).toBe(1)
+    // Low pos object
+    expect(slideBar.state('LowPos').Value).toBe(low)
+    expect(slideBar.state('LowPos').Label).toBe(`${low}`)
+    // expect(slideBar.state('LowPos').Percentage).toBe(0)
 
-  // Other state parameters
-  const stepRange = _.range(min, max + step, step)
+    // High pos object
+    expect(slideBar.state('HighPos').Value).toBe(high)
+    expect(slideBar.state('HighPos').Label).toBe(`${high}`)
+    // expect(slideBar.state('HighPos').Percentage).toBe(1)
 
-  expect(slideBar.state('dragging')).toBe(null)
-  expect(slideBar.state('Steps').length).toBe(stepRange.length)
-  expect(slideBar.state('dragOffset')).toBe(0)
-  expect(slideBar.state('startPos')).toBe(0)
-  expect(slideBar.state('eventType')).toBe(null)
+    // Other state parameters
+    const stepRange = _.range(min, max + step, step)
+
+    expect(slideBar.state('dragging')).toBe(null)
+    expect(slideBar.state('Steps').length).toBe(stepRange.length)
+    expect(slideBar.state('dragOffset')).toBe(0)
+    expect(slideBar.state('startPos')).toBe(0)
+    expect(slideBar.state('eventType')).toBe(null)
+  })
 })
